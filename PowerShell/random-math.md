84307ceaa7fcc84daa6b10064c187028890f6c43
Jude Chen <judechen2006@gmail.com>
2019-03-16 11:07:48 -0500

    Added md file, added lower limit of the additions.

---
+++ PowerShell/random-math.md
@@ -0,0 +1,21 @@
+# How to print the generated txt file
+
+## Windows
+
+1. Open the txt file with Notepad.
+
+2. From the "Format" menu, select "Font...".
+
+3. Set the "Font" to "Arial", "Font Style" to "Bold" and "Size" to 18.
+
+    ![Font Window](../images/notepad_font.png "Font Settings")
+
+4. From the "File" menu, select "Page Setup...".
+
+5. Set the "Orientataion" to "Landscape" and click "OK".
+
+    ![Page Setup window](../images/notepad_page_setup.png "Page Setup")
+
+6. From the "File" menu, select "Print..." (or press Ctrl-P)
+
+7. Select a printer to print.
--- PowerShell/random-math.ps1
+++ PowerShell/random-math.ps1
@@ -1,15 +1,17 @@
-﻿[int]$max = Read-Host -Prompt "Enter the upper limit of the exercises "
-[int]$total = Read-Host -Prompt "Enter total number of the questions "
+﻿[int]$max = Read-Host -Prompt "Enter the upper limit of the additions "
+[int]$min = Read-Host -Prompt "Enter the lower limit of the additions "
+[int]$pages = Read-Host -Prompt "Enter the number of pages "
 [int]$chance = Read-Host -Prompt "Enter the chance of the showups of the upper limit (1 and above, larger number lower chance) "
 
-$filename = "Addition_${max}_total_${total}.txt"
+$filename = "Addition_${min}_to_${max}_pages_${pages}.txt"
+$total = $pages * 48
 $i = 1
 $output = ""
 $null | Out-File $filename -Force
 
 While ($i -le $total) {
 
-    $chance_random = Get-Random -Maximum ($chance+1) -Minimum 1
+    $chance_random = Get-Random -Maximum ($chance + 1) -Minimum 1
 
     Switch ($chance_random) {
         $chance {
@@ -36,7 +38,7 @@ While ($i -le $total) {
             while ($true) {
                 $num1 = Get-Random -Maximum $max -Minimum 1
                 $num2 = Get-Random -Maximum $max -Minimum 1
-                If ($num1 + $num2 -le $max) {
+                If (($num1 + $num2 -le $max) -and ($num1 + $num2 -ge $min)) {
                     if ($i % 4 -eq 0) {
                         $output += "$num1 + $num2 = `t"
                         Write-Output "$output `r" |Out-File $filename -Append
